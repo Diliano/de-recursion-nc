8a0f2cda57133a0013d2418397e5ff12c7e90b26
Dilan Patel <113666014+Diliano@users.noreply.github.com>
2024-09-16 12:08:45 +0100

    refactor sum_int()

--- src/sum_int.py
+++ src/sum_int.py
@@ -1,9 +1,5 @@
 def sum_int(num):
-    if num == 0:
-        return 0
+    if num == 0 or num == 1:
+        return num
     
-    if num == 1:
-        return 1
-    
-    step_num = num - 1
-    return num + sum_int(step_num)
+    return num + sum_int(num - 1)
